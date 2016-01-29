d835d857576c9ddccc5876a928744878f1c373a9
bairedzhang <200801323@qq.com>
2016-01-29 11:56:51 +0800

    up

--- index.js
+++ index.js
@@ -38,14 +38,17 @@ global.log = function () {
 };
 let project = {
     projects: {},
+
     init: function () {
         this.proxy();
     },
+
     add: function (config, method) {
         method = method || 'watch';
         let project = this.projects[config.name] = new Project(config);
         project[method]();
     },
+
     watch: function (config) {
         let name = config.name;
         if (!this.projects[name]) {
@@ -54,27 +57,41 @@ let project = {
             this.projects[name].watch();
         }
     },
+
     proxy: function (config) {
         proxy.start(config);
     },
+
     closeProxy: function () {
         proxy.close();
     },
+
     close: function (config) {
         this.projects[config.name].close();
     },
+
     closeAll: function () {
         Object.keys(this.projects).forEach(function (name) {
             this.projects[name].close();
         }.bind(this));
         this.closeProxy();
     },
+
     build: function (config) {
         let name = config.name;
         if (!this.projects[name]) {
             this.add(config, 'build');
         } else {
             this.projects[name].build();
+        }
+    },
+
+    compile: function (config) {
+        let name = config.name;
+        if (!this.projects[name]) {
+            this.add(config, 'compile');
+        } else {
+            this.projects[name].compile();
         }
     }
 };
