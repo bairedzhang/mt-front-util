bc53c0646a4d9ddf4a7a87c52c0467c6e6208c24
bairedzhang <200801323@qq.com>
2016-04-29 23:27:13 +0800

    support config in ~/.mtrc

--- bin/mt.js
+++ bin/mt.js
@@ -6,12 +6,12 @@ const program = require('commander');
 const Path = require('path');
 const cwd = Path.resolve(process.cwd());
 const confPath = cwd + '/mt.conf';
-const jsfmt = require('jsfmt');
 const newProject = require('../lib/init');
 const util = require('../lib/util');
+const config = require('../lib/config');
 const pkg = util.readJSON(Path.join(__dirname, '../package.json'));
-const mtConfig = util.readJSON(Path.join(__dirname, '../.mt'));
 const version = pkg.version;
+const mtConfig = require('../lib/config').getConfig();
 
 program
     .version(version)
@@ -21,35 +21,26 @@ program
      init           脚手架 init projectName [jsp/ftl] 默认jsp
      compile        编译整个项目到本地代理路径`)
     .option('-p --proxy', '本地代理')
-    .option('--user [type]', 'config user')
 
 program.parse(process.argv);
 
 let method = program.args[0] || 'watch';
 
 let setConfig = function (cwd, confPath) {
-    let config = util.readJSON(confPath);
+    let conf = util.readJSON(confPath);
     let serverPath = '';
     if (/frontend/g.test(cwd)) {
         serverPath = cwd.replace(/.*?(\/frontend)/, '$1');
     }
-    if (/frontend\/(?!mods)/g.test(cwd)) {
-        config.frontendModsPath = cwd.replace(/^(.*frontend\/).*$/g, '$1mods');
-    }
-    config = Object.assign({}, {
+    conf = Object.assign({}, {
         localPath: Path.resolve(cwd),
         serverRoot: `/usr/local/app/resin_${mtConfig.user}/webapps`,
         proxyRoot: mtConfig.proxyRoot,
         serverPath: serverPath,
         cssProPath: './static/'
-    }, config);
-    return config;
+    }, conf);
+    return conf;
 };
-
-if (program.user) {
-    mtConfig.user = program.user;
-    util.writeJSON(Path.join(__dirname, '../.mt'), JSON.stringify(mtConfig));
-}
 
 if (method == 'init') {
     new newProject(program.args[1], program.args[2] || '').init();
@@ -58,9 +49,14 @@ if (method == 'init') {
 if (fs.existsSync(confPath)) {
     let config = setConfig(cwd, confPath);
     mt.add(config, method);
-    if (method == 'watch' && fs.existsSync(config.frontendModsPath)) {
-        let frontConf = setConfig(config.frontendModsPath, config.frontendModsPath + '/mt.conf');
-        mt.add(frontConf, 'watch');
+    let localMods = mtConfig.localFrontendMods;
+    if (method == 'watch') {
+        if (fs.existsSync(localMods)) {
+            let frontConf = setConfig(localMods, localMods + '/mt.conf');
+            mt.add(frontConf, 'watch');
+        } else {
+            log(`can't find mods in your local, please config your localFrontendMods in ${process.env.HOME}/.mtrc`.red);
+        }
     }
     if (program.proxy) {
         mt.proxy(config);
